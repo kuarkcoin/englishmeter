5d2833806ca0ec39a7618dfdd3a767a3ecda0f33
kuarkcoin <30313951+kuarkcoin@users.noreply.github.com>
2025-11-22 21:04:25 +0300

    Update page.tsx

--- app/race/[id]/page.tsx
+++ app/race/[id]/page.tsx
@@ -7,7 +7,7 @@ export default async function RacePage({ params }: { params: { id: string } }) {
   
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
+ 
   // 1. Güvenlik Kontrolü
   if (!supabaseUrl || !supabaseKey) {
     return (
