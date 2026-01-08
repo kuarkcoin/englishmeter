e909e279f65507cae4b6e08e62d50e7e13819235
kuarkcoin <30313951+kuarkcoin@users.noreply.github.com>
2026-01-09 02:20:22 +0300

    Update page.tsx

--- app/yds-3750/page.tsx
+++ app/yds-3750/page.tsx
@@ -5,7 +5,7 @@ import React, { useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import type { Variants } from 'framer-motion';
-import ydsVocabulary from '@/data/yds_vocabulary_.json';
+import ydsVocabulary from '@/data/yds_vocabulary.json';
 
 type VocabItem = {
   word: string;
